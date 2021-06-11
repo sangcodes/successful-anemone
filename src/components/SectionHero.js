68e3e402a8c3a258af22743a69447255a9378340
Stackbit Code Editor <projects@stackbit.com>
2021-06-11 01:44:24 +0000

    SectionHero.js: changed

--- src/components/SectionHero.js
+++ src/components/SectionHero.js
@@ -18,8 +18,10 @@ export default class SectionHero extends React.Component {
                   )}
                   <div className="cell block-content">
                     {_.get(section, 'title', null) && (
+                        <>
                     <h2>Welcome to</h2>
                     <h2 className="block-title underline">{_.get(section, 'title', null)}</h2>
+                    </>
                     )}
                     <div className="block-copy">
                       {markdownify(_.get(section, 'content', null))}
